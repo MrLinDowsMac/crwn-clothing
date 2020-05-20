ae32d98908534fa71f137549ed2f29249ac578ec
Ian Nájar <ianenri@gmail.com>
2020-05-20 16:40:58 -0600

    Agregado redirect cuando usuario esta logueado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -55,14 +55,18 @@ class App extends React.Component{
         <Route exact path='/' component={HomePage} />
         {/* <Route path='/hats' component={HatsPage} /> */}
         <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUp} />
+        <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to="/"/>) : (<SignInAndSignUp/>) }/>
       </Switch>
     </div>)
   }
 }
 
+const mapStateToProps = ({ user }) => ({ //userReducer
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,11 +3,14 @@ import { Link } from "react-router-dom";
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux'; //High Order Component modify our component to allow us use redux
 
-import { ReactComponent as Logo } from "../../assets/original.svg";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo' />
@@ -24,13 +27,16 @@ const Header = ({ currentUser }) => (
                 <div className='option' onClick={ () => auth.signOut() }>SIGN OUT</div> :
                 <Link className='option' to='/signin'>SIGN IN</Link>
             }
+            <CartIcon />
         </div>
+        { hidden ? null : <CartDropdown /> }
     </div>
 );
 
-const mapStateToProps = state => ({ //state is root reducer
-    currentUser: state.user.currentUser
-})
+const mapStateToProps = ( { user: {currentUser}, cart: { hidden }} ) =>  ({ //state is root reducer
+    currentUser,
+    hidden
+});
 
 export default connect(mapStateToProps)(Header); //first arg is the state of reducer
 //export default Header;
