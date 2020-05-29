7c6f7cf22d2e3c4540ded89b4a57cb5e3fad60f9
Ian Nájar <ianenri@gmail.com>
2020-05-29 01:17:26 -0600

    Agregado redux-persist para persistencia del carrito

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store  from './redux/store';
+import {store, persistor}  from './redux/store';
+
+import { PersistGate } from "redux-persist/integration/react";
+
 import './index.css';
 import App from './App';
 //import * as serviceWorker from './serviceWorker';
@@ -12,9 +15,11 @@ import App from './App';
 ReactDOM.render(
   <Provider store={store}>
   <BrowserRouter>
+   <PersistGate persistor={persistor}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
+   </PersistGate>
   </BrowserRouter>
   </Provider>,
   document.getElementById('root')
